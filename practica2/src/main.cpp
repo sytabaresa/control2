253ceec14f9c4b9bd60b98de136187352da05f00
Sebastian Yesid Tabares Amaya <sytabaresa@gmail.com>
2019-05-10 17:58:16 -0500

    fix copy of registers -_-

--- practica2/src/main.cpp
+++ practica2/src/main.cpp
@@ -40,29 +40,31 @@ uint8_t pot = 0;
  * constBoverB0 = [b0/a0, b1/a0, b2/a0, ... , bn/a0] (length n ==> NUM = n)
  */
 #define Ts TrMax / 10
-#define A0 (1.0 + 6 * Ts)
+#define CUERPO int32_t
 
-#define DEM 2
-const double consAoverA0[DEM] = {1.0, -1.0 / A0};
-double state[DEM];
+#define DEM 3
+// const CUERPO consA[DEM] = {A0, -100};
+const CUERPO consA[DEM] = {100, 116, 36};
+CUERPO state[DEM];
 
-#define NUM 2
-const double consBoverA0[NUM] = {-(6.0 + 4 * Ts) / A0, 6.0 / A0};
+#define NUM 3
+// const CUERPO consB[NUM] = {-630, 600};
+const CUERPO consB[NUM] = {0, 138, 72};
 //const double consBoverA0[NUM] = {(-0.6780383795309168349), (0.01279317697228144965)};
-double error[NUM];
+CUERPO error[NUM];
 
 void output(uint8_t inValue, uint8_t outValue, int16_t inInternal, int16_t outInternal)
 {
 
   lcd.clear();
   lcd.setCursor(0, 0);
-  lcd.print("in: ");
-  inValue < 10 ? lcd.print(000) : 0;
-  inValue < 100 ? lcd.print(00) : 0;
-  inValue < 1000 ? lcd.print(0) : 0;
-  lcd.print(inValue);
-  lcd.print(" ");
-  lcd.print(inInternal);
+  // lcd.print("in: ");
+  // inValue < 10 ? lcd.print(000) : 0;
+  // inValue < 100 ? lcd.print(00) : 0;
+  // inValue < 1000 ? lcd.print(0) : 0;
+  // lcd.print(inValue);
+  // lcd.print(" ");
+  // lcd.print(inInternal);
 
   pot = analogRead(POT) >> 2;
   pot = map(pot, 0, 255, 1, 100);
@@ -70,23 +72,18 @@ void output(uint8_t inValue, uint8_t outValue, int16_t inInternal, int16_t outIn
   lcd.print(pot);
   pot < 10 ? lcd.print(" ") : 0;
 
-  lcd.setCursor(0, 1);
-  lcd.print("out: ");
-  lcd.print(outValue);
-  lcd.print(" ");
-  lcd.print(outInternal);
-  outValue = map(outValue, 0, 255, 255, 0);
+  // lcd.setCursor(0, 1);
+  // lcd.print("out: ");
+  // lcd.print(outValue);
+  // lcd.print(" ");
+  // lcd.print(outInternal);
+  // outValue = map(outValue, 0, 255, 255, 0);
 
   for (int i = 7; i >= 0; i--)
   {
     uint8_t v = (outValue >> i) & 0x01;
     digitalWrite(outPins[i], v);
   }
-}
-
-double doubleMap(double x, double in_min, double in_max, double out_min, double out_max)
-{
-  return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
 }
 
 /**
@@ -119,60 +116,6 @@ void setup()
 
 void loop()
 {
-  // e[k] readed
-  uint16_t in = analogRead(IN);
-
-#define OFFIN 1000
-  error[0] = doubleMap(in, 0, 1023, -OFFIN, OFFIN);
-#ifdef DEBUG
-  Serial.print("in1: ");
-  Serial.print(in);
-  Serial.print(", in2: ");
-  Serial.println(error[0]);
-#endif
-  // u[k] calc
-
-  //STATE
-  for (int i = 1; i < DEM; i++)
-  {
-    state[0] += consAoverA0[i] * state[i];
-#ifdef DEBUG
-    Serial.print("st");
-    Serial.print(i);
-    Serial.print(": ");
-    Serial.println(consAoverA0[i] * state[i]);
-#endif
-  }
-
-  //ERROR
-  for (int i = 0; i < NUM; i++)
-  {
-    state[0] += consBoverA0[i] * error[i];
-#ifdef DEBUG
-    Serial.print("err");
-    Serial.print(i);
-    Serial.print(": ");
-    Serial.println(consBoverA0[i] * error[i]);
-#endif
-  }
-
-// output u[k]
-//#define OFFOUT 1000
-  uint8_t out = map(state[0], -OFFIN, OFFIN, 0, 255);
-#ifdef DEBUG
-  Serial.print("out1: ");
-  Serial.print(state[0]);
-  Serial.print(", in2: ");
-  Serial.println(out);
-#endif
-  output(in, out, error[0], state[0]);
-
-  // Update registers to new values
-  for (int i = 0; i < DEM - 1; i++)
-    state[i + 1] = state[i];
-
-  for (int i = 0; i < NUM - 1; i++)
-    error[i + 1] = error[i];
 
 #ifdef DEBUG
   Serial.print("state: ");
@@ -188,11 +131,80 @@ void loop()
     Serial.print(error[i]);
     Serial.print(", ");
   }
+
+#endif
+
+  // e[k] readed
+  uint16_t in = analogRead(IN);
+
+#define OFFIN 200
+#define LIM 10000000
+  error[0] = map(in, 0, 1023, OFFIN, -OFFIN);
+#ifdef DEBUG
+  Serial.print("in: ");
+  Serial.print(in);
+  Serial.print(", e[k]: ");
+  Serial.println(error[0]);
+#endif
+  // u[k] calc
+
+  //STATE
+  for (int i = 1; i < DEM; i++)
+  {
+    state[0] -= consA[i] * state[i];
+#ifdef DEBUG
+    Serial.print("state");
+    Serial.print(i);
+    Serial.print(": ");
+    Serial.println(consA[i] * state[i]);
+#endif
+  }
+
+  //ERROR
+  for (int i = 0; i < NUM; i++)
+  {
+    state[0] += consB[i] * error[i];
+#ifdef DEBUG
+    Serial.print("error");
+    Serial.print(i);
+    Serial.print(": ");
+    Serial.println(consB[i] * error[i]);
+#endif
+  }
+
+  state[0] = state[0] > LIM ? LIM : state[0];
+  state[0] = state[0] < -LIM ? -LIM : state[0];
+  state[0] /= consA[0];
+
+  // output u[k]
+  //#define OFFOUT 1000
+  uint8_t out = map(state[0], -OFFIN, OFFIN, 0, 255);
+#ifdef DEBUG
+  Serial.print("out1: ");
+  Serial.print(state[0]);
+  Serial.print(", in2: ");
+  Serial.println(out);
+#endif
+  output(in, out, error[0], state[0]);
+
+  // Update registers to new values
+  for (int i = DEM - 1; i > 0; i--)
+    state[i] = state[i - 1];
+
+  for (int i = NUM - 1; i > 0; i--)
+    error[i] = error[i - 1];
+
+#ifdef DEBUG
+  Serial.print("out: ");
+  Serial.println(out);
   Serial.println();
   Serial.println();
 
 #endif
+  state[0] = 0;
+  error[0] = 0;
+
   // Ts delay
-delay(TrMax / pot);
- // delay(1000);
+  delay(TrMax / pot);
+  // delay(1000);
 }
