ca47ec94629c9a2ba897df217550eb4ea54ceb34
Sebastian Yesid Tabares Amaya <sytabaresa@gmail.com>
2019-06-21 20:45:27 -0500

    encoder logic with debounce

---
+++ practica3/include/Encoder.h
@@ -0,0 +1,71 @@
+#ifndef __ENCODER_H__
+#define __ENCODER_H__
+
+#define DISTANCE 219
+
+// constructor : sets pins as inputs and turns on pullup resistors
+static boolean rotating = false; // debounce management
+
+volatile uint16_t position = 0;
+
+int8_t pin_a;
+
+int8_t pin_b;
+
+volatile boolean A_set = false;
+
+volatile boolean B_set = false;
+
+void setEncoder(int8_t PinA, int8_t PinB)
+{
+  pin_a = PinA;
+  pin_b = PinB;
+
+  // set pin a and b to be input
+  pinMode(pin_a, INPUT);
+  pinMode(pin_b, INPUT);
+  // turn on pullup resistors
+  digitalWrite(pin_a, HIGH);
+  digitalWrite(pin_b, HIGH);
+};
+
+// call this from your interrupt function
+// Interrupt on A changing state
+void doEncoderA()
+{
+  // debounce
+  if (rotating)
+    delay(1); // wait a little until the bouncing is done
+
+  // Test transition, did things really change?
+  if (digitalRead(pin_a) != A_set)
+  { // debounce once more
+    A_set = !A_set;
+
+    // adjust counter + if A leads B
+    if (A_set && !B_set)
+      // position = position % DISTANCE + 1;
+      position++;
+
+    rotating = false; // no more debouncing until loop() hits again
+  }
+}
+
+// Interrupt on B changing state, same as A above
+void doEncoderB()
+{
+  if (rotating)
+    delay(1);
+  if (digitalRead(pin_b) != B_set)
+  {
+    B_set = !B_set;
+    //  adjust counter - 1 if B leads A
+    if (B_set && !A_set)
+      // position = position == 0 ? DISTANCE : position;
+      position--;
+
+    rotating = false;
+  }
+}
+
+#endif // __ENCODER_H__
--- practica3/src/main.cpp
+++ practica3/src/main.cpp
@@ -5,12 +5,16 @@
  */
 
 #include <Arduino.h>
+#include "Encoder.h"
+
+// Encoder encoder(2, 4);
 
 // analog input in
 #define CURRENTpin A6
 #define SENSORpin A0
 
 #define INpin A2
+#define BUTTONpin 12
 
 #define ENpin 7
 #define PWMpin 9
@@ -23,6 +27,7 @@
 
 #define CUERPO double
 
+uint8_t button = 0;
 uint16_t out, in, in2 = 0;
 CUERPO in1, ref, error = 0;
 
@@ -31,6 +36,14 @@ CUERPO in1, ref, error = 0;
  */
 void setup()
 {
+  // encoder pin on interrupt 0 (pin 2)
+  attachInterrupt(0, doEncoderA, CHANGE);
+
+  // encoder pin on interrupt 1 (pin 3)
+  attachInterrupt(1, doEncoderB, CHANGE);
+
+  setEncoder(2, 3);
+
 #ifdef DEBUG
   // Serial begin
   Serial.begin(115200);
@@ -41,6 +54,7 @@ void setup()
   pinMode(SENSORpin, INPUT);
   pinMode(CURRENTpin, INPUT);
   pinMode(INpin, INPUT);
+  pinMode(BUTTONpin, INPUT_PULLUP);
 
   pinMode(ENpin, OUTPUT);
   pinMode(PWMpin, OUTPUT);
@@ -50,44 +64,50 @@ void setup()
   digitalWrite(ENpin, HIGH);
 }
 
-void output(uint8_t in)
+void output(int16_t value)
 {
-  in = in > LIM ? LIM : in;
-  in = in < -LIM ? -LIM : in;
+  value = value > LIM ? LIM : value;
+  value = value < -LIM ? -LIM : value;
 
-  digitalWrite(OUTApin, in > 0);
-  digitalWrite(OUTBpin, in < 0);
+  digitalWrite(OUTApin, value > 0);
+  digitalWrite(OUTBpin, value < 0);
 
-  in2 = map(abs(in), 0, 100, 0, 255);
-  analogWrite(PWMpin, in2);
+  value = map(abs(value), 0, LIM, 0, 255);
+  analogWrite(PWMpin, value);
 }
-
-
 
 void smartDelay(unsigned long us)
 {
   unsigned long fin = micros() + us;
   do
   {
+    button = digitalRead(BUTTONpin);
+    if (!button)
+    {
+      output(0);
+      out = ref = 0;
+    }
+    rotating = true;
 
 #ifdef DEBUG
-  double t = in * 0.02434;
-  Serial.print(in);
-  Serial.print(",");
-  Serial.print(in1);
-  Serial.print(",");
-  Serial.print(ref);
-  Serial.print(",");
-  Serial.print(error);
-  Serial.print(",");
-  Serial.print(out);
-  Serial.print(",");
-  Serial.println(t);
+    double t = in * 0.02434;
+    Serial.print(in);
+    Serial.print(",");
+    Serial.print(in1);
+    Serial.print(",");
+    Serial.print(ref);
+    Serial.print(",");
+    Serial.print(error);
+    Serial.print(",");
+    Serial.print(out);
+    Serial.print(",");
+    Serial.print(t);
+    Serial.print(",");
+    Serial.println(position);
 #endif
   } while (micros() < fin);
   return;
 }
-
 
 /*
 *
@@ -108,9 +128,9 @@ void loop()
     ref = Serial.parseInt();
   }
 #endif
-  error = ref - in1;
-
-  out = map(K * error, -LIM, LIM, 0, 256);
+  // error = ref - in1;
+  // out = K * error;
+  out = ref;
   output(out);
 
   // Serial.print("in2: ");
